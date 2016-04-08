07c948e5905505e3dee5f15e97317cdeef61fc2b
Alex Mayfield <alexmax2742@gmail.com>
2016-04-07 21:36:59 -0400

    Use assert.NoError() instead of explicit nil error checks.

--- xpnode/database_test.go
+++ xpnode/database_test.go
@@ -8,51 +8,51 @@ import (
 
 func TestNewDatabase(t *testing.T) {
 	_, err := NewDatabase(":memory:")
-	if err != nil {
-		t.Error("%s", err.Error())
-	}
+	assert.NoError(t, err)
 }
 
 func TestGet(t *testing.T) {
 	db, err := NewDatabase(":memory:")
-	if err != nil {
-		t.Error(err.Error())
+	if !assert.NoError(t, err) {
+		t.FailNow()
 	}
 
 	err = db.Import("../fixture/zanxp.sql")
-	if err != nil {
-		t.Fatal(err.Error())
+	if !assert.NoError(t, err) {
+		t.FailNow()
 	}
 
 	row, err := db.Get("alexmax")
-	if err != nil {
-		t.Fatal(err.Error())
+	if assert.NoError(t, err) {
+		assert.Equal(t, "alexmax", row.Name, "Should return proper row name")
+		assert.Equal(t, StringInt32(359450), row.Experience, "Should return proper row experience")
+		assert.Equal(t, StringFloat64(1459903084.82901), row.Timestamp, "Should return proper row timestamp")
 	}
-	assert.Equal(t, "alexmax", row.Name, "Should return proper row name")
-	assert.Equal(t, StringInt32(359450), row.Experience, "Should return proper row experience")
-	assert.Equal(t, StringFloat64(1459903084.82901), row.Timestamp, "Should return proper row timestamp")
 
 	row, err = db.Get("anonymous")
-	if err != nil {
-		t.Fatal(err.Error())
+	if assert.NoError(t, err) {
+		assert.Nil(t, row, "Should be nil")
 	}
-	assert.Nil(t, row, "Should be nil")
 }
 
 func TestGetSince(t *testing.T) {
 	db, err := NewDatabase(":memory:")
-	if err != nil {
-		t.Error(err.Error())
+	if !assert.NoError(t, err) {
+		t.FailNow()
 	}
 
 	err = db.Import("../fixture/zanxp.sql")
-	if err != nil {
-		t.Fatal(err.Error())
+	if !assert.NoError(t, err) {
+		t.FailNow()
 	}
 
 	rows, err := db.GetSince(1459903084.82901)
-	if err != nil {
-		t.Fatal(err.Error())
+	if assert.NoError(t, err) {
+		assert.Equal(t, 2, len(rows), "Should return two values")
 	}
-	assert.Equal(t, 2, len(rows), "Should return two values")
+
+	rows, err = db.GetSince(2000000000)
+	if assert.NoError(t, err) {
+		assert.Equal(t, 0, len(rows), "Should return no values")
+	}
 }
