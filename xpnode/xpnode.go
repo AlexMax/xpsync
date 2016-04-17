ce70cf6a31029fe6e66a5697a6188fa512ac526a
Alex Mayfield <alexmax2742@gmail.com>
2016-04-17 14:34:29 -0400

    Initial stab at RPC.

--- xpnode/xpnode.go
+++ xpnode/xpnode.go
@@ -1,12 +1,68 @@
 package main
 
-import "log"
+import (
+	"log"
+	"net"
+	"net/rpc"
+)
 
-func main() {
+type App struct {
+	db     *Database
+	server *rpc.Server
+}
+
+// Create new App
+func NewApp() (app *App, err error) {
+	app = &App{}
+
 	filename := "zanxp.db"
 
-	_, err := NewDatabase(filename)
+	app.db, err = NewDatabase(filename)
 	if err != nil {
-		log.Fatalf("could not open database: %s", filename)
+		return
 	}
+
+	app.server = rpc.NewServer()
+
+	messages := new(Messages)
+	app.server.Register(messages)
+
+	return
+}
+
+// Serve application
+func (app *App) ListenAndServe() (err error) {
+	listener, err := net.Listen("tcp", ":9876")
+	if err != nil {
+		return
+	}
+	defer listener.Close()
+
+	log.Printf("Now listening on %s", listener.Addr().String())
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			return err
+		}
+
+		app.server.ServeConn(conn)
+	}
+
+	return
+}
+
+func main() {
+	app, err := NewApp()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	log.Fatal(app.ListenAndServe())
+}
+
+type Messages int
+
+func (t *Messages) FullUpdate(_ struct{}, reply *[]Experience) (err error) {
+	return
 }
---
+++ xpnode/xpnode_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/rpc"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewApp(t *testing.T) {
+	_, err := NewApp()
+	assert.NoError(t, err)
+}
+
+func TestFullUpdate(t *testing.T) {
+	app, err := NewApp()
+	if !assert.NoError(t, err) {
+		t.FailNow()
+	}
+
+	go app.ListenAndServe()
+
+	time.Sleep(time.Second)
+
+	client, err := rpc.Dial("tcp", "localhost:9876")
+	if !assert.NoError(t, err) {
+		t.FailNow()
+	}
+
+	var nothing struct{}
+	var xps []Experience
+	err = client.Call("Messages.FullUpdate", nothing, &xps)
+	if !assert.NoError(t, err) {
+		t.FailNow()
+	}
+	assert.Equal(t, 5, len(xps))
+}
